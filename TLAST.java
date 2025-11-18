8ff1b3105cea8e610b42bafa9ed9cf1eaba0b94d
Tohonan Silaban <silabandit1@gmail.com>
2025-11-18 21:17:02 +0700

    Toho
    DONE

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,41 @@
-//Nama - NIM
+//Tohonan Silaban - 12S25036 //
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+    
+    public static double prio(double tingkatkesulitan, double hariHinggaDeadline) {
+        double prioritas;
+        String prio;
+
+        tingkatkesulitan = Double.parseDouble(input.nextLine());
+        hariHinggaDeadline = Double.parseDouble(input.nextLine());
+        prioritas = tingkatkesulitan * 1.0 / hariHinggaDeadline;
+        if (prioritas > 3) {
+            prio = "Penting! Anda harus mengerjakan tugas ini segera";
+        } else {
+            if (prioritas <= 3 && prioritas >= 1.5) {
+                prio = "Tugas ini memiliki prioritas menengah";
+            } else {
+                if (prioritas > 1.5) {
+                    prio = "Tugas ini relatif ringan, namun jangan tunda terlalu lama";
+                }
+            }
+        }
+        System.out.println(prio);
+        
+        return prioritas;
+    }
+    
+    public static String task(String[] add) {
+        String h;
+
+        
+        return h;
+    }
+}
 
